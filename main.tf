663f7d7c8fe9d18d7c2193663b8f56bb374e416a
saraMir26 <mirz0014@algonquinlive.com>
2026-06-13 23:47:35 -0400

    add output values

--- main.tf
+++ main.tf
@@ -159,3 +159,13 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  description = "Public IP address of the web server"
+  value       = azurerm_public_ip.public_ip.ip_address
+}
