3d3884c72a43dbb9d544ab739df69e6a2ceaf8f8
saraMir26 <mirz0014@algonquinlive.com>
2026-06-13 23:45:51 -0400

    add linux virtual machine

--- main.tf
+++ main.tf
@@ -127,3 +127,35 @@ data "cloudinit_config" "init" {
     content      = file("${path.module}/init.sh")
   }
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  size           = "Standard_B1s"
+  admin_username = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  custom_data = data.cloudinit_config.init.rendered
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
