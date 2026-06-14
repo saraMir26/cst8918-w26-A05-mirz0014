a0385363361b893a3e597271ac7f0eea8e31de59
saraMir26 <mirz0014@algonquinlive.com>
2026-06-13 23:06:57 -0400

    associate nsg with nic

--- main.tf
+++ main.tf
@@ -112,3 +112,8 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
